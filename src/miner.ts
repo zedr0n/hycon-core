300459a1cbfd057113c9e337218b1b0cdc154ae1
Jimin Kim <KJM@glosfer.com>
2018-04-20 14:46:17 +0900

    Edit miner API.

--- src/miner.ts
+++ src/miner.ts
@@ -2,8 +2,7 @@ type Tx = any; // TODO: define type
 type Block = any; // TODO: define type
 type BlockMinedCallback = (block: Block) => void;
 export interface IMiner {
-    newTopTxs(topTxs: Tx[]): void;
-    newTopBlock(topBlock: Block): void;
+    newCandidateBlock(topBlock: Block): void;
     start(): void;
     stop(): void;
     addCallbackNewBlock(callback: BlockMinedCallback, priority?: number): void;
