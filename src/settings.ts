bec27e292af97c0906048d70f401944621e7a2a8
Owen <owen@glosfer.com>
2018-04-30 16:26:21 +0900

    Lint

--- src/settings.ts
+++ src/settings.ts
@@ -1,10 +1,10 @@
 export = {
-    "dataRoot": "./data",
-    "dataRaw": "./data/raw",
-    "dataMeta": "./data/meta",
-    "dataWallet": "./wallet",
-    "dataAccount": "./data/accounts",
-    "dataGenesis": "./data/genesis.dat",
-    "dataBlockFilePrefix": "./data/raw/blk",
-    "dagFile": "./data/dag_hycon.png"
+    dagFile: "./data/dag_hycon.png",
+    dataAccount: "./data/accounts",
+    dataBlockFilePrefix: "./data/raw/blk",
+    dataGenesis: "./data/genesis.dat",
+    dataMeta: "./data/meta",
+    dataRaw: "./data/raw",
+    dataRoot: "./data",
+    dataWallet: "./wallet",
 }
