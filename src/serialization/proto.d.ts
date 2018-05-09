fef8f2bb5ab4fe1a3c870a77d14b1819d2b6261f
Owen <owen@glosfer.com>
2018-05-09 16:16:01 +0900

    protogen

--- src/serialization/proto.d.ts
+++ src/serialization/proto.d.ts
@@ -2793,6 +2793,114 @@ export class TxDB implements ITxDB {
     public toJSON(): { [k: string]: any };
 }
 
+/** Properties of a TxList. */
+export interface ITxList {
+
+    /** TxList tx */
+    tx?: ITx;
+
+    /** TxList previousFrom */
+    previousFrom?: Uint8Array;
+
+    /** TxList previousTo */
+    previousTo?: Uint8Array;
+
+    /** TxList blockHash */
+    blockHash?: Uint8Array;
+}
+
+/** Represents a TxList. */
+export class TxList implements ITxList {
+
+    /**
+     * Constructs a new TxList.
+     * @param [properties] Properties to set
+     */
+    constructor(properties?: ITxList);
+
+    /** TxList tx. */
+    public tx?: ITx;
+
+    /** TxList previousFrom. */
+    public previousFrom: Uint8Array;
+
+    /** TxList previousTo. */
+    public previousTo: Uint8Array;
+
+    /** TxList blockHash. */
+    public blockHash: Uint8Array;
+
+    /**
+     * Creates a new TxList instance using the specified properties.
+     * @param [properties] Properties to set
+     * @returns TxList instance
+     */
+    public static create(properties?: ITxList): TxList;
+
+    /**
+     * Encodes the specified TxList message. Does not implicitly {@link TxList.verify|verify} messages.
+     * @param message TxList message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encode(message: ITxList, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Encodes the specified TxList message, length delimited. Does not implicitly {@link TxList.verify|verify} messages.
+     * @param message TxList message or plain object to encode
+     * @param [writer] Writer to encode to
+     * @returns Writer
+     */
+    public static encodeDelimited(message: ITxList, writer?: $protobuf.Writer): $protobuf.Writer;
+
+    /**
+     * Decodes a TxList message from the specified reader or buffer.
+     * @param reader Reader or buffer to decode from
+     * @param [length] Message length if known beforehand
+     * @returns TxList
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decode(reader: ($protobuf.Reader|Uint8Array), length?: number): TxList;
+
+    /**
+     * Decodes a TxList message from the specified reader or buffer, length delimited.
+     * @param reader Reader or buffer to decode from
+     * @returns TxList
+     * @throws {Error} If the payload is not a reader or valid buffer
+     * @throws {$protobuf.util.ProtocolError} If required fields are missing
+     */
+    public static decodeDelimited(reader: ($protobuf.Reader|Uint8Array)): TxList;
+
+    /**
+     * Verifies a TxList message.
+     * @param message Plain object to verify
+     * @returns `null` if valid, otherwise the reason why it is not
+     */
+    public static verify(message: { [k: string]: any }): (string|null);
+
+    /**
+     * Creates a TxList message from a plain object. Also converts values to their respective internal types.
+     * @param object Plain object
+     * @returns TxList
+     */
+    public static fromObject(object: { [k: string]: any }): TxList;
+
+    /**
+     * Creates a plain object from a TxList message. Also converts values to other types if specified.
+     * @param message TxList
+     * @param [options] Conversion options
+     * @returns Plain object
+     */
+    public static toObject(message: TxList, options?: $protobuf.IConversionOptions): { [k: string]: any };
+
+    /**
+     * Converts this TxList to JSON.
+     * @returns JSON object
+     */
+    public toJSON(): { [k: string]: any };
+}
+
 /** Properties of a BlockHeader. */
 export interface IBlockHeader {
 
