53c94cf071c94770ca82133deac885bbb78b3eae
Francis Lim <thyeem@gmail.com>
2018-05-09 16:59:16 +0900

    (tmp) test with mining

--- src/util/difficulty.ts
+++ src/util/difficulty.ts
@@ -1,20 +1,30 @@
+import { getLogger } from "log4js"
 
 let lastDifficulty: number = 10000
 const timeArr: number[] = []
 const dTime: number[] = []
 let ema: number = 30000
-const weight: number = 0.3
+const weight: number = 0.8
+const adjacent: number = 10
 const target: number = 30000
+// tslint:disable-next-line:no-var-requires
+const dataDiff: number[][] = []
+const upperLimit = 4
+const plot = require("gnu-plot")()
+const logger = getLogger("difficulty")
 
 // tslint:disable:no-bitwise
 function updateDifficulty(newDifficulty: number): void {
     lastDifficulty = newDifficulty
+    const float = unforcedInt(newDifficulty)
+    logger.debug(`Diff: ${float}`)
+    plotDiff(float)
 }
 
 function updateTimes(timestamp: number): void {
     timeArr.length === 0 ? dTime.push(target) : dTime.push(timestamp - timeArr[timeArr.length - 1])
     timeArr.push(timestamp)
-    if (timeArr.length > 30) {
+    if (timeArr.length > 10) {
         timeArr.shift()
         dTime.shift()
     }
@@ -22,13 +32,13 @@ function updateTimes(timestamp: number): void {
 }
 
 function calcEMA(): number {
-    const multiplier = 1. / (30 + 1)
+    const multiplier = 1. / (10 + 1)
     const size = dTime.length
     const emas: number[] = []
 
     for (let i = 0; i < size; i++) {
         // Using Formula: EMA = (weight; 2/(N+1)) * (current - prevEMA) + prevEMA
-        if (i === 0) { emas[0] = dTime[0] } else if (i > 0 && i < 30) {
+        if (i === 0) { emas[0] = dTime[0] } else if (i > 0 && i < 10) {
             let sum = 0
             for (let j = 0; j <= i; j++) { sum += dTime[j] }
             emas[i] = sum / (i + 1)
@@ -37,6 +47,19 @@ function calcEMA(): number {
         }
     }
     return emas[emas.length - 1]
+}
+
+function plotDiff(value: number) {
+    plot.set({
+        grid: "ture", xrange: "[0: ]", yrange: `[0:${upperLimit + 0.3}]`,
+    })
+    dataDiff.push([++dataDiff.length, value])
+    plot.plot([{
+        color: "red",
+        data: dataDiff,
+        style: "linespoints",
+        title: "Difficulty",
+    }])
 }
 
 export function processBlock(diff: number, timestamp: number = Date.now()): void {
