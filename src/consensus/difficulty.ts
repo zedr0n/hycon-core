d764b34067b2c8b18cb7b07e893428299a72414e
Josiah Evans <josiah.g.e@gmail.com>
2018-05-10 17:41:08 +0900

    Adding lessThan functionality to difficulty

--- src/consensus/difficulty.ts
+++ src/consensus/difficulty.ts
@@ -1,24 +1,98 @@
 import { Hash } from "../util/hash"
 
+// tslint:disable:no-bitwise
 export class Difficulty {
 
-    public static decode(num: number): Difficulty {
-        return new Difficulty(0, 0)
+    public static decode(num: Uint8Array): Difficulty {
+        const exponent = num[0]
+        let mantissa = 0
+
+        for (let i = 1; i < 4; i++) {
+            mantissa = mantissa << 8
+            mantissa = mantissa + num[i]
+        }
+
+        return new Difficulty(mantissa, exponent)
     }
-    private e: number
+
+    public static pack(num: Uint8Array): Uint8Array {
+        let msb = 0
+        for (let i = 31; i > 2 ; i--) {
+            if (num[i] > 0) {
+                msb = i
+                break
+            }
+        }
+
+        const mantissa = new Uint8Array([0, 0, 0])
+        if (msb > 2) {
+            for (let i = 0; i < 2; i++) {
+                mantissa[i] = num[msb - i]
+            }
+        } else {
+            for (let i = 2; i >= 0; i--) {
+                mantissa[i] = num[i]
+            }
+        }
+
+        const packedBytes = new Uint8Array(4)
+        packedBytes[0] = msb
+        for (let i = 1; i < 4; i++) {
+            packedBytes[i] = mantissa[i]
+        }
+
+        return packedBytes
+    }
     private m: number
 
-    constructor(e: number, m: number) {
-        this.e = e
-        this.m = m
+    private e: number
+
+    constructor(mantissa?: number, exponent?: number, byteArray?: Uint8Array) {
+        if ((mantissa !== undefined) && (exponent !== undefined)) {
+            this.m = mantissa
+            this.e = exponent
+        } else if (byteArray !== undefined) {
+            if (byteArray.length === 32) {
+                return Difficulty.decode(Difficulty.pack(byteArray))
+            } else if (byteArray.length === 4) {
+                return Difficulty.decode(byteArray)
+            }
+        } else {
+            return new Difficulty(0, 0)
+        }
     }
 
-    public encode(): number {
-        return 0
+    public encode(): Uint8Array {
+
+        const packed = new Uint8Array([0, 0, 0, 0])
+        packed[0] = this.e
+        for (let i = 1; i < 4; i++) {
+            packed[i] = this.m >> ((3 - i) * 8)
+        }
+        return packed
     }
 
     public lessThan(hash: Hash): boolean {
-        return false
+
+        const packedHash = Difficulty.pack(hash)
+        const packedDifficulty = this.encode()
+
+        const exponentCompare = packedDifficulty[0] < packedHash[0]
+        let mantissaCompare = false
+        for (let i = 2; i >= 0; i--) {
+            if (packedDifficulty[i] < packedHash[i]) {
+                mantissaCompare = true
+                break
+            }
+        }
+
+        if (mantissaCompare && exponentCompare) {
+            return true
+        } else if (packedHash[0] > 0 && exponentCompare) {
+            return true
+        } else {
+            return false
+        }
     }
 
     public multiply(num: number) {
--- test/difficulty.spec.ts
+++ test/difficulty.spec.ts
@@ -1,26 +1,96 @@
 import { Difficulty } from "../src/consensus/difficulty"
+import { Hash } from "../src/util/hash"
 
-xdescribe("Difficulty", () => {
+describe("Difficulty", () => {
     let difficulty: Difficulty
 
     it("decode: should decode a 4 byte number into a Difficulty object", () => {
-        expect(false).toBe(true)
+        const packedNumber = new Uint8Array([1, 2, 3, 4])
+        difficulty = Difficulty.decode(packedNumber)
+        const repackedNumber = difficulty.encode()
+        expect(repackedNumber).toEqual(packedNumber)
     })
 
     it("constructor: should create a difficulty object without generating a runtime error", () => {
-        difficulty = new Difficulty(1, 2)
+        difficulty = new Difficulty(1, 0)
         expect(difficulty).toBeDefined()
     })
 
-    it("encode: should encode itself as a 4 byte number", () => {
-        expect(false).toBe(true)
+    it("encode: should encode the mantissa in the proper position", () => {
+        // 0x00_00_01
+        difficulty = new Difficulty(1, 0)
+        let encodedDifficulty = difficulty.encode()
+        let correctEncodedDifficulty = new Uint8Array([0, 0, 0, 1])
+        expect(encodedDifficulty).toEqual(correctEncodedDifficulty)
+
+        // 0x00_01_00
+        difficulty = new Difficulty(256, 0)
+        encodedDifficulty = difficulty.encode()
+        correctEncodedDifficulty = new Uint8Array([0, 0, 1, 0])
+        expect(encodedDifficulty).toEqual(correctEncodedDifficulty)
+
+        // 0x01_00_00
+        difficulty = new Difficulty(65536, 0)
+        encodedDifficulty = difficulty.encode()
+        correctEncodedDifficulty = new Uint8Array([0, 1, 0, 0])
+        expect(encodedDifficulty).toEqual(correctEncodedDifficulty)
     })
 
-    it("lessThan: should return true if the hash is larger than the difficulty value", () => {
-        expect(false).toBe(true)
+    it("encode: should encode the exponent in the proper position", () => {
+        difficulty = new Difficulty(0, 1)
+        let encodedDifficulty = difficulty.encode()
+        let correctEncodedDifficulty = new Uint8Array([1, 0, 0, 0])
+        expect(encodedDifficulty).toEqual(correctEncodedDifficulty)
+
+        difficulty = new Difficulty(0, 16)
+        encodedDifficulty = difficulty.encode()
+        correctEncodedDifficulty = new Uint8Array([16, 0, 0, 0])
+        expect(encodedDifficulty).toEqual(correctEncodedDifficulty)
     })
 
-    it("multiply: should return the product of the difficulty value and the number", () => {
+    it("lessThan: should return true if the difficulty value is less than the given hash", () => {
+        difficulty = new Difficulty(10, 0)
+        const bigHashBytes = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0, 0, 0,
+                                           0, 0, 0, 0, 0, 0, 0, 11])
+        const bigHash = new Hash(bigHashBytes)
+        const compare = difficulty.lessThan(bigHash)
+        expect(compare).toEqual(true)
+    })
+
+    it("lessThan: should return false if the difficulty is greater than the given hash", () => {
+        difficulty = new Difficulty(10, 0)
+        const littleHashBytes = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0,
+                                                0, 0, 0, 0, 0, 0, 0, 0,
+                                                0, 0, 0, 0, 0, 0, 0, 0,
+                                                0, 0, 0, 0, 0, 0, 0, 9])
+        const littleHash = new Hash(littleHashBytes)
+        const compare = difficulty.lessThan(littleHash)
+        expect(compare).toEqual(false)
+    })
+
+    it("pack: should properly pack a Uint8Array to 4 bytes", () => {
+        const hashBytes = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 9])
+        const packedHashBytes = Difficulty.pack(hashBytes)
+        const correctHashBytes = new Uint8Array([0, 0, 0, 9])
+        expect(packedHashBytes).toEqual(correctHashBytes)
+    })
+
+    xit("lessThanEq: should return true if the difficulty is equal to the given hash", () => {
+        difficulty = new Difficulty(10, 0)
+        const equalHashBytes = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 0,
+                                               0, 0, 0, 0, 0, 0, 0, 10])
+        const equalHash = new Hash(equalHashBytes)
+
+    })
+
+    xit("multiply: should return the product of the difficulty value and the number", () => {
         expect(false).toBe(true)
     })
 
