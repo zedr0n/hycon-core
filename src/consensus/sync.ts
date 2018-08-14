7a84b312d456d5cc49f4981e4bbd2db5358950e4
Unknown <zedr0nre@gmail.com>
2018-08-14 16:21:23 +0100

    change sync race

--- src/consensus/sync.ts
+++ src/consensus/sync.ts
@@ -25,21 +25,31 @@ class EliminationRace<T> {
         promise.catch((e) => this.remove(promise))
     }
 
+    private conditional<T>(promises: Promise<T>[], condition: (v: T) => boolean) {
+        return Promise.all<T>(promises.map(p => {
+            // If a request fails, count that as a resolution so it will keep
+            // waiting for other possible successes. If a request succeeds,
+            // treat it as a rejection so Promise.all immediately bails out.
+
+            return p.then(
+                v => {
+                    if (condition(v))
+                        return Promise.reject<T>(v)
+                    else
+                        return Promise.resolve<T>(undefined)
+                },
+                e => Promise.resolve(e)
+            )
+        })).then(
+            // If '.all' resolved, we've just got an array of errors.
+            e => Promise.resolve<T[]>([]),
+            // If '.all' rejected, we've got the result we wanted.
+            v => Promise.resolve<T[]>([v])
+        ).catch();
+    }
+
     public race(test: (t: T) => boolean = () => true) {
-        return new Promise<T>(async (resolve, reject) => {
-            while (this.promises.length > 0) {
-                try {
-                    const p = Promise.race(this.promises)
-                    const value = await p
-                    if (test(value)) {
-                        resolve(value)
-                    }
-                } catch (e) {
-                    logger.debug(e)
-                }
-            }
-            reject(`No promise resolved`)
-        })
+        return this.conditional<T>(this.promises, v => test(v))
     }
 
     private remove(promise: Promise<T>) {
@@ -96,8 +106,12 @@ export class Sync {
                 promise.add(peer.getBTip().then((tip) => ({ peer, tip })).catch((e) => logger.debug(e)))
             }
             const filter = (peer: ICandidatePeer) => peer !== undefined && peer.tip !== undefined && (peer.tip.totalwork > this.consensus.getHtip().totalWork || peer.tip.height > this.consensus.getHtip().height)
-            const { peer, tip } = await promise.race(filter)
-            await peer.headerSync(tip)
+            const reply = await promise.race(filter)
+            if (reply.length > 0) {
+                const { peer, tip } = reply[0]
+                logger.info(`Syncing headers from peer with tip ${tip.height}, ${tip.totalwork > this.consensus.getHtip().totalWork ? `${tip.totalwork} > ${this.consensus.getHtip().totalWork}` : `${tip.height} > ${this.consensus.getBtip().height}`}`)
+                await peer.headerSync(tip)
+            }
         } catch (e) {
             logger.debug(e)
         }
@@ -111,12 +125,16 @@ export class Sync {
             for (const peer of await this.network.getPeers()) {
                 promise.add(peer.getBTip().then((tip) => ({ peer, tip })).catch((e) => logger.debug(e)))
             }
-            const filter = (peer: ICandidatePeer) => peer !== undefined && peer.tip !== undefined && peer.tip.totalwork > this.consensus.getBtip().totalWork
-            const { peer, tip } = await promise.race(filter)
-            if (peer.getVersion() > 5) {
-                await peer.txSync(tip)
-            } else {
-                await peer.blockSync(tip)
+            const filter = (peer: ICandidatePeer) => peer !== undefined && peer.tip !== undefined && (peer.tip.totalwork > this.consensus.getBtip().totalWork || peer.tip.height > this.consensus.getBtip().height)
+            const reply = await promise.race(filter)
+            if (reply.length > 0) {
+                const { peer, tip } = reply[0]
+                logger.info(`Syncing blocks from peer with tip ${tip.height}, ${tip.totalwork > this.consensus.getBtip().totalWork ? `${tip.totalwork} > ${this.consensus.getBtip().totalWork}` : `${tip.height} > ${this.consensus.getBtip().height}`}`)
+                if (peer.getVersion() > 5) {
+                    await peer.txSync(tip)
+                } else {
+                    await peer.blockSync(tip)
+                }
             }
         } catch (e) {
             logger.debug(e)
