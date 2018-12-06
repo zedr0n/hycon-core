913c183e56215fdab73127399fa0bc959e033ab6
Unknown <zedr0nre@gmail.com>
2018-12-06 18:38:00 +0000

    increase timeout

--- src/network/peer.ts
+++ src/network/peer.ts
@@ -792,7 +792,7 @@ export class Peer extends BasePeer {
                 const wait = setTimeout(() => {
                     clearTimeout(wait);
                     resolve([])
-                }, 3000)
+                }, 10000)
             })
             blocks = await Promise.race([this.getBlocksByRange(height, MAX_BLOCKS_PER_PACKET), timeout])
             if (height < maxHeight && blocks.length == 0) {
